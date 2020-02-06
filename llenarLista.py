c8f1814c1dfdef5a3e76076de69254b0eda61d37
sebasped <sebasped@disroot.org>
2020-02-06 15:04:56 -0300

    modificado llenarLista.py con un for dentro de la funciones

--- llenarLista.py
+++ llenarLista.py
@@ -3,18 +3,20 @@ import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
-def agregarUno(lista):
-    lista.append(1)
+def agregarPares(lista):
+    for i in range(100):
+        lista.append(2*i)
     
-def agregarDos(lista):
-    lista.append(2)
+def agregarImpares(lista):
+    for i in range(100):
+        lista.append(2*i+1)
 
 
 lista = []
 
-for i in range(25):
-    t1 = threading.Thread(target=agregarUno, args=[lista])
-    t2 = threading.Thread(target=agregarDos, args=[lista])
+for i in range(20):
+    t1 = threading.Thread(target=agregarPares, args=[lista])
+    t2 = threading.Thread(target=agregarImpares, args=[lista])
     t1.start()
     t2.start()
 
